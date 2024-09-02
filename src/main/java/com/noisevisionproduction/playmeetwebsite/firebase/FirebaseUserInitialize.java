dab3595e75d99818c26af92e40c94de577dd24b5
Noise290 <noisemonster29@gmail.com>
2024-09-02 21:41:23 +0200

    testing if id token is correct in firebase

--- src/main/java/com/noisevisionproduction/playmeetwebsite/firebase/FirebaseUserInitialize.java
+++ src/main/java/com/noisevisionproduction/playmeetwebsite/firebase/FirebaseUserInitialize.java
@@ -1,6 +1,7 @@
 package com.noisevisionproduction.playmeetwebsite.firebase;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class FirebaseUserInitialize {
         this.firebaseAuth = firebaseAuth;
     }
 
-    public FirebaseToken verifyToken(String idToken) throws Exception {
-        return FirebaseAuth.getInstance().verifyIdToken(idToken);
+    public FirebaseToken verifyToken(String idToken) throws FirebaseAuthException {
+        return firebaseAuth.verifyIdToken(idToken);
     }
 }
